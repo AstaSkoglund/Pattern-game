4a0b241677f600872639e4f864b195cd12a29a33
Diogenis <vcurran@seattleacademy.org>
2024-01-29 12:52:10 -0800

    V2

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 const color=document.getElementById('colordrop');
-const height=document.getElementById('hight');
+const height=document.getElementById('height');
 const width=document.getElementById('width');
 const rebuild = document.getElementById('rebuild');
 const square = document.getElementById('square');
 rebuild.addEventListener('click', rebuildfunction); 
 var colorvalue = 'aqua';
 
-function rebuildfunction(){
-    colorvalue = colordrop.value;
-    square.style.backgroundColor = colorvalue;
-    for ( i = 0; i < height.value; r++){
-        let row = document.createElement("div");
-        square.appendChild(row).className = "square";
+
+    function rebuildfunction(){
+        colorvalue = colordrop.value;
+        square.style.backgroundColor = colorvalue;
+    for (let i = 0; i < height.value; i++) {
+        for (let r = 0; r < width.value; r++) {
+          const gridItem = document.createElement('div');
+          gridItem.className = 'asquare';   
+          square.appendChild(gridItem);
+        }
     }
 }
 
 
-
+/*
+    function rebuildfunction(){
+        colorvalue = colordrop.value;
+        square.style.backgroundColor = colorvalue;
+        for ( i = 0; i < height.value; i++){
+            square.appendChild('div')
+            makerow
+    
+    
+        }
+    }
+    function makerow(){
+    
+        for ( i = 0; i < width.value; i++){    
+        let boxy = document.createElement("div");
+        square.appendChild(boxy).className = "square";
+        }
+    }
+*/
