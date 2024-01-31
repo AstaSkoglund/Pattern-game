6adedcce6bae89b70ecf6be79d6a86606dc8ec47
Diogenis <vcurran@seattleacademy.org>
2024-01-31 10:30:45 -0800

    V3

--- script.js
+++ script.js
@@ -8,15 +8,25 @@ var colorvalue = 'aqua';
 
 
     function rebuildfunction(){
+        let gridItem;
         colorvalue = colordrop.value;
         square.style.backgroundColor = colorvalue;
     for (let i = 0; i < height.value; i++) {
         for (let r = 0; r < width.value; r++) {
-          const gridItem = document.createElement('div');
-          gridItem.className = 'asquare';   
+          gridItem = document.createElement('div');
+          console.log(i,r); 
+          gridItem.style.left = ((i*50)+150).toString() + 'px';
+          gridItem.style.top = ((r*50)+50).toString() + 'px';
+          gridItem.className = 'asquare';
+          gridItem.addEventListener('click', changecolor); 
           square.appendChild(gridItem);
         }
     }
+
+}
+function changecolor(event){
+    colorvalue = colordrop.value;
+    event.target.style.backgroundColor = colorvalue;
 }
 
 
